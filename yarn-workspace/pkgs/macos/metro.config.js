6682944dcedc4bc5e63021f56aa210bee8737e9b
Abhishek Kumar <abhishek.karak@gmail.com>
2022-07-22 22:15:58 -0700

    First commit with ios, android, and macos support.

---
+++ yarn-workspace/pkgs/macos/metro.config.js
@@ -0,0 +1,32 @@
+/**
+ * Metro configuration for React Native
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+const exclusionList = require("metro-config/src/defaults/blacklist");
+const { getMetroTools } = require("react-native-monorepo-tools");
+
+// Get the metro settings to make it compatible with Yarn workspaces.
+const monorepoMetroTools = getMetroTools({
+  reactNativeAlias: "react-native-macos",
+});
+
+module.exports = {
+  transformer: {
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: false,
+      },
+    }),
+  },
+  // Add additional Yarn workspace package roots to the module map.
+  // This allows importing from any workspace.
+  watchFolders: monorepoMetroTools.watchFolders,
+  resolver: {
+    // Ensure we resolve nohoist libraries from this directory.
+    blacklistRE: exclusionList(monorepoMetroTools.blockList),
+    extraNodeModules: monorepoMetroTools.extraNodeModules,
+  },
+};
